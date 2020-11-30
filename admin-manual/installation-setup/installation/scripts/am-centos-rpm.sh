aea7021b4f9dee4916e9dd72300d6676e5f737d6
Jesús García Crespo <jesus@sevein.com>
2020-11-30 20:47:03 +0100

    Update more version refs

--- admin-manual/installation-setup/installation/scripts/am-centos-rpm.sh
+++ admin-manual/installation-setup/installation/scripts/am-centos-rpm.sh
@@ -37,9 +37,9 @@ EOF'
 sudo -u root bash -c 'cat << EOF > /etc/yum.repos.d/archivematica-extras.repo
 [archivematica-extras]
 name=archivematica-extras
-baseurl=https://packages.archivematica.org/1.12.x/centos-extras
+baseurl=https://packages.archivematica.org/1.13.x/centos-extras
 gpgcheck=1
-gpgkey=https://packages.archivematica.org/1.12.x/key.asc
+gpgkey=https://packages.archivematica.org/1.13.x/key.asc
 enabled=1
 EOF'
 
