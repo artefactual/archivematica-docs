a059df87cb08fdb5b10d20da52ea59d90aed8388
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2023-11-29 18:50:26 +0100

    Release AM 1.15.1 + SS 0.21.1

--- conf.py
+++ conf.py
@@ -78,7 +78,7 @@ copyright = u'2023, Artefactual Systems Inc.'
 # The short X.Y version.
 version = '1.15'
 # The full version, including alpha/beta/rc tags.
-release = '1.15.0'
+release = '1.15.1'
 
 rst_epilog = """
 .. |previous_version| replace:: 1.14
